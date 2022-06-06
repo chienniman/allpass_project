175d009911b27e68d7140c8e2f57c46ac5d1b161
chingchieh98 <103157715+chingchieh98@users.noreply.github.com>
2022-06-06 09:25:02 +0800

    品牌故事圖片文字修改

--- JS/feedback.js
+++ JS/feedback.js
@@ -1,68 +1,37 @@
+jQuery.validator.methods.matches = function (value, element, params) {
+    var re = new RegExp(params);
+    return this.optional(element) || re.test(value);
+};
 
-// show a message with a type of the input
-function showMessage(input, message, type) {
-	// get the small element and set the message
-	const msg = input.parentNode.querySelector("small");
-	msg.innerText = message;
-	// update the class for the input
-	input.className = type ? "success" : "error";
-	return type;
-} 
+var re = new RegExp("^09\\d{8}$");
+$( "#form" ).validate({
+	rules: {
+		phone: {
+			required: true,
+			matches: re, // <-- no such method called "matches"!
+		},
+		name: {
+			required: true,
+		},
+		suggestion: {
+			required: true,
+		},
+  	},
 
-function showError(input, message) {
-	return showMessage(input, message, false);
-}
+	messages: {
+		phone: {
+			required: "請填寫您的手機號碼",
+			matches: "您的手機號碼格式錯誤",
+		},
+		name: {
+			required: "請填寫您的姓名",
+		},
+		suggestion: {
+			required: "請填寫建議內容",
+		},
+	},
 
-function showSuccess(input) {
-	return showMessage(input, "", true);
-}
-
-function hasValue(input, message) {
-	if (input.value.trim() === "") {
-		return showError(input, message);
-	}
-	return showSuccess(input);
-}
-
-function validateEmail(input, requiredMsg, invalidMsg) {
-	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
-		return false;
-	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	const email = input.value.trim();
-	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
-	}
-	return true;
-}
-
-const form = document.querySelector("#form");
-
-const NAME_REQUIRED = "請輸入姓名";
-// const EMAIL_REQUIRED = "請輸入信箱";
-const EMAIL_INVALID = "請確認信箱格式是否正確";
-const SUGGESTION_REQUIRED = "請輸入建議內容";
-
-
-form.addEventListener("submit", function (event) {
-	// stop form submission
-	event.preventDefault();
-	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_INVALID);
-    let suggestionValid = hasValue(form.elements["suggestion"], SUGGESTION_REQUIRED);
-	// if valid, submit the form.
-	if (nameValid && emailValid && suggestionValid) {
-		swal({
-			title: "已經成功收到您寶貴的意見回復",
-			text: "感謝您",
-			icon: "success",
-			button: "關閉",
-		  });
+	submitHandler: function (form) {
+		alert('成功送出！');
 	}
 });
-
--- JS/story.js
+++ JS/story.js
@@ -3,37 +3,37 @@ AOS.init({
     duration: 1200,
 });
 
-function show1() {
-    var hide = document.getElementById('hide1');
+new WOW().init();
 
-    if (hide.style.display === 'none') {
-        hide.style.display = 'block';
-    }
-    else {
-        hide.style.display = 'none';
-    }
-} 
+// function show1() {
+//     var hide = document.getElementById('hide1');
 
-function show2() {
-    var hide = document.getElementById('hide2');
+//     if (hide.style.display === 'none') {
+//         hide.style.display = 'block';
+//     }
+//     else {
+//         hide.style.display = 'none';
+//     }
+// } 
 
-    if (hide.style.display === 'none') {
-        hide.style.display = 'block';
-    }
-    else {
-        hide.style.display = 'none';
-    }
-} 
+// function show2() {
+//     var hide = document.getElementById('hide2');
 
-function show3() {
-    var hide = document.getElementById('hide3');
+//     if (hide.style.display === 'none') {
+//         hide.style.display = 'block';
+//     }
+//     else {
+//         hide.style.display = 'none';
+//     }
+// } 
 
-    if (hide.style.display === 'none') {
-        hide.style.display = 'block';
-    }
-    else {
-        hide.style.display = 'none';
-    }
-} 
+// function show3() {
+//     var hide = document.getElementById('hide3');
 
-
+//     if (hide.style.display === 'none') {
+//         hide.style.display = 'block';
+//     }
+//     else {
+//         hide.style.display = 'none';
+//     }
+// } 
