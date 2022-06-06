7ff9a1d8cf861d1a091eccffd9dca2f1953d1f91
齊尼曼 <cscs851210max@gmail.com>
2022-06-06 14:09:22 +0800

    人氣餐點檢查點2

--- JS/index.js
+++ JS/index.js
@@ -18,14 +18,14 @@ var swiper = new Swiper(".mySwiper", {
 
 var swiper1 = new Swiper(".mySwiper1", {
   loop: true,
-  spaceBetween: 50,
+  spaceBetween: 300,
   slidesPerView: 2,
   freeMode: true,
   watchSlidesProgress: true,
-  // autoplay: {
-  //   delay:1000,
-  //   disableOnInteraction: false,
-  // },
+  autoplay: {
+    delay:2000,
+    disableOnInteraction: false,
+  },
 });
 var swiper2 = new Swiper(".mySwiper2", {
   loop: false,
